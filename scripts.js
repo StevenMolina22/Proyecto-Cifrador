2f7378dd68cb07c7fc3e5e940755318c1fd50879
Steven Molina <steven.molina.slayer@gmail.com>
2024-02-26 01:11:11 -0300

    project working not pulished

--- scripts.js
+++ scripts.js
@@ -1,116 +1,81 @@
-// function mainfunction(){
-//   // alert("its working mf");
-//   const message1 = prompt("write here lil nigga");
-//   const lenghtMessage1 = message1.length
-//   let message1Array = [];
-//   let encryptedMessageArray = [];
-//   let encryptedMessage;
-//   const vocals = ['a', 'e', 'i', 'o', 'u'];
-//   const keyWords = ["ai", "enter", "imes", "ober", "ufat"]
-//   let keys = {
-//     'a':"ai",
-//     'e':"enter",
-//     'i':"imes",
-//     'o':"ober",
-//     'u':"ufat"
-//   }
-//   let keys2 = {
-//     "ai": 'a',
-//     "enter": 'e',
-//     "imes": 'i',
-//     "ober": 'o',
-//     "ufat": 'u'
-//   }
-//   // alert(`your fucking message was: ${message1}  beautiful nigga`);
-//   for (let i=0; i < lenghtMessage1; i++){
-//     // alert(message1[i]);
-//     message1Array.push(message1[i]);
-//   }
-//   // alert(message1Array);
-//   // alert(`the key is: ${keys["u"]}`);
+function showInfo() {
+  // containers
+  const sideContentCrypted = document.querySelector("#sideContentCrypted");
+  const sideContentEmpty = document.querySelector("#sideContentEmpty");
+  // text
+  const sideContentText = document.querySelector("#sideContentText");
+  if (sideContentText.innerHTML == "") {
+    sideContentCrypted.classList.toggle("hidden");
+    sideContentEmpty.classList.toggle("hidden");
+  }
+}
 
-//   for (let i = 0; i < lenghtMessage1; i++){
-//     if (vocals.includes(message1Array[i])){
-//       encryptedMessageArray.push(keys[message1Array[i]]);
-//     }
-//     else {
-//       encryptedMessageArray.push(message1Array[i]);
-//     }
-//   }
-//   encryptedMessage = encryptedMessageArray.join("")
-//   alert(`the codified message1 is: ${encryptedMessage}`);
-
-//   let message2 = prompt("write your message: ");
-  
-//   for (const item of keyWords){
-//     if (message2.includes(item)) {
-//       // alert(`the keyword is: ${item}`);
-//       // alert(`the item is: ${keys2[item]}`);
-//       // alert(`the message is ${message2.replace(item, keys2[item])}`)
-//       message2 = message2.replace(item, keys2[item]);
-//     }
-//   }
-//   alert(`the decrypted message is: ${message2}`);
-// }
-
-
-
-
-function encryptInfo(){
+function encryptInfo() {
+  showInfo();
   // dom selection
   const labelInput = document.querySelector("#inputMessage");
   const labelInputValue = labelInput.value;
+  const sideContentText = document.querySelector("#sideContentText");
   // variables and const for later
   let message1 = labelInputValue;
   const lenghtMessage1 = message1.length;
   let message1Array = [];
   let encryptedMessageArray = [];
   let encryptedMessage;
-  const vocals = ['a', 'e', 'i', 'o', 'u'];
+  const vocals = ["a", "e", "i", "o", "u"];
   let keys = {
-    'a':"ai",
-    'e':"enter",
-    'i':"imes",
-    'o':"ober",
-    'u':"ufat"
-  }
+    a: "ai",
+    e: "enter",
+    i: "imes",
+    o: "ober",
+    u: "ufat",
+  };
   // transform message str to array
-  for (let i=0; i < lenghtMessage1; i++){
+  for (let i = 0; i < lenghtMessage1; i++) {
     message1Array.push(message1[i]);
   }
   // encrypts the message by vocals
-  for (let i = 0; i < lenghtMessage1; i++){
-    if (vocals.includes(message1Array[i])){
+  for (let i = 0; i < lenghtMessage1; i++) {
+    if (vocals.includes(message1Array[i])) {
       encryptedMessageArray.push(keys[message1Array[i]]);
-    }
-    else {
+    } else {
       encryptedMessageArray.push(message1Array[i]);
     }
   }
   // transform the array into str
-  encryptedMessage = encryptedMessageArray.join("")
-  alert(`encrypted message: ${encryptedMessage}`);
+  encryptedMessage = encryptedMessageArray.join("");
+  sideContentText.innerHTML = encryptedMessage;
+  // alert(`encrypted message: ${encryptedMessage}`);
 }
 
-function decryptInfo(){
+function decryptInfo() {
+  showInfo();
   const labelInput2 = document.querySelector("#inputMessage");
   const labelInput2Value = labelInput2.value;
+  const sideContentText = document.querySelector("#sideContentText");
   // alert(labelInput2Value);
 
   const keyWords = ["ai", "enter", "imes", "ober", "ufat"];
   let keys2 = {
-    "ai": 'a',
-    "enter": 'e',
-    "imes": 'i',
-    "ober": 'o',
-    "ufat": 'u'
-  }
+    ai: "a",
+    enter: "e",
+    imes: "i",
+    ober: "o",
+    ufat: "u",
+  };
 
   let message2 = labelInput2Value;
-  for (const item of keyWords){
+  for (const item of keyWords) {
     if (message2.includes(item)) {
       message2 = message2.replaceAll(item, keys2[item]);
     }
   }
-  alert(`decrypted message: ${message2}`);
+  sideContentText.innerHTML = message2;
+  // alert(`decrypted message: ${message2}`);
+}
+
+function copyTextToClipBoard() {
+  const sideContentText = document.querySelector("#sideContentText");
+  navigator.clipboard.writeText(sideContentText.innerHTML);
+  alert("Copied!");
 }
